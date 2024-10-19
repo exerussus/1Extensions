723de0ddbaa8dac30c4770569b53679eb351e417
exerussus <solovyov.production@gmail.com>
2024-10-20 02:20:24 +0300

    Auto updated to version 1.0.13

--- SignalSystem/SignalHandler.cs
+++ SignalSystem/SignalHandler.cs
@@ -1,16 +1,15 @@
 ﻿
+using Plugins.Exerussus._1Extensions.Abstractions;
 using UnityEngine;
 
 namespace Exerussus._1Extensions.SignalSystem
 {
     [CreateAssetMenu(menuName = "1Extensions/SignalHandler", fileName = "SignalHandler")]
-    public class SignalHandler : ScriptableObject
+    public class SignalHandler : ScriptableObject, IInitializable
     {
         public Signal Signal { get; private set; } = new Signal();
 
-        private void Awake()
-        {
-            Signal = new();
-        }
+        private void Awake() { Initialize(); }
+        public void Initialize() { Signal = new(); }
     }
 }
