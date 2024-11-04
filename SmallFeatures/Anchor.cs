a196c71af5c5e193c32c272143c8171925564986
exerussus <solovyov.production@gmail.com>
2024-11-05 00:58:12 +0300

    Auto updated to version 1.0.22

---
+++ SmallFeatures/Anchor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace Exerussus._1Extensions.SmallFeatures
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = true, AllowMultiple = true), Conditional("UNITY_EDITOR")]
+    public class AnchorAttribute : PropertyAttribute
+    {
+        
+    }
+
+}
