bb1be92944888a698c87eadd0b509927e1becfa4
exerussus <solovyov.production@gmail.com>
2024-11-07 04:36:53 +0300

    Auto updated to version 1.0.24

---
+++ ScriptBuilder/EnumCreator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Exerussus._1Extensions.ScriptBuilding
+{
+    public class EnumCreator
+    {
+        private string _name;
+        private List<string> _values = new();
+        private List<string> _attributes = new();
+        private ScriptBuilder _scriptCreator;
+
+        public string Name => _name;
+        public List<string> Values => _values;
+
+        private EnumCreator(ScriptBuilder scriptCreator, string name)
+        {
+            _scriptCreator = scriptCreator;
+            _name = name;
+        }
+
+        public static EnumCreator Create(ScriptBuilder scriptCreator, string name)
+        {
+            return new EnumCreator(scriptCreator, name);
+        }
+
+        public EnumCreator AddAttribute(string attribute)
+        {
+            _attributes.Add(attribute);
+            return this;
+        }
+
+        public EnumCreator AddValue(string value)
+        {
+            _values.Add(value);
+            return this;
+        }
+
+        public ScriptBuilder End()
+        {
+            return _scriptCreator;
+        }
+
+        public override string ToString()
+        {
+            var enumBuilder = new StringBuilder();
+
+            foreach (var attribute in _attributes)
+            {
+                enumBuilder.AppendLine($"    [{attribute}]");
+            }
+
+            enumBuilder.AppendLine($"    public enum {_name}");
+            enumBuilder.AppendLine("    {");
+
+            for (int i = 0; i < _values.Count; i++)
+            {
+                enumBuilder.AppendLine(i == _values.Count - 1
+                    ? $"        {_values[i]}"
+                    : $"        {_values[i]},");
+            }
+
+            enumBuilder.AppendLine("    }");
+
+            return enumBuilder.ToString();
+        }
+    }
+}
--- ScriptBuilder/ScriptBuilder.cs
+++ ScriptBuilder/ScriptBuilder.cs
@@ -7,6 +7,7 @@ namespace Exerussus._1Extensions.ScriptBuilding
     {
         private string _nameSpace;
         private List<ClassCreator> _classes = new();
+        private List<EnumCreator> _enums = new();
         private List<string> _usings = new();
 
         private ScriptBuilder(string nameSpace)
@@ -30,6 +31,13 @@ namespace Exerussus._1Extensions.ScriptBuilding
             var classCreator = ClassCreator.Create(this, name);
             _classes.Add(classCreator);
             return classCreator;
+        }
+
+        public EnumCreator AddEnum(string name)
+        {
+            var enumCreator = EnumCreator.Create(this, name);
+            _enums.Add(enumCreator);
+            return enumCreator;
         }
 
         public override string ToString()
@@ -56,6 +64,15 @@ namespace Exerussus._1Extensions.ScriptBuilding
                 var classCreator = _classes[index];
                 if (index > 0) scriptBuilder.AppendLine("");
                 scriptBuilder.Append(classCreator);
+            }
+            
+            scriptBuilder.AppendLine("");
+            
+            for (var index = 0; index < _enums.Count; index++)
+            {
+                var enumCreator = _enums[index];
+                if (index > 0) scriptBuilder.AppendLine("");
+                scriptBuilder.Append(enumCreator);
             }
 
             scriptBuilder.AppendLine("}");
