71488c1ebb88d9992678929f243ddad7a47ac392
exerussus <solovyov.production@gmail.com>
2024-11-11 01:53:29 +0300

    Auto updated to version 1.0.27

---
+++ SmallFeatures/Tracer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Exerussus._1Extensions.SmallFeatures
+{
+    public static class Tracer
+    {
+        private static string _prefix = ">>>>>>>>>>>>>>> ";
+        
+        public static void Start(string prefix)
+        {
+            _prefix = prefix;
+            Debug.Log($"{_prefix}Start");
+        }
+
+        public static void Ping(int message)
+        {
+            Debug.Log($"{_prefix}{message}");
+        }
+
+        public static void Ping(string message)
+        {
+            Debug.Log($"{_prefix}{message}");
+        }
+    }
+}
