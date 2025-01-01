760ade6fbe66b3a687a9c6959e3843212d7a368a
exerussus <solovyov.production@gmail.com>
2025-01-01 22:47:13 +0300

    Auto updated to version 1.0.48

---
+++ Prefabs/PrefabsExtensions.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+namespace Exerussus._1Extensions.Prefabs
+{
+    public static class PrefabUtilityExtensions
+    {
+        public static void RevertToPrefab(GameObject gameObject)
+        {
+#if UNITY_EDITOR
+
+            if (UnityEditor.PrefabUtility.IsPartOfPrefabInstance(gameObject))
+            {
+                GameObject prefabRoot = UnityEditor.PrefabUtility.GetOutermostPrefabInstanceRoot(gameObject);
+                if (prefabRoot != null)
+                {
+                    UnityEditor.PrefabUtility.RevertPrefabInstance(prefabRoot, UnityEditor.InteractionMode.UserAction);
+                    Debug.Log($"Сброшены изменения для объекта {gameObject.name} до состояния префаба.");
+                }
+                else
+                {
+                    Debug.LogWarning($"Не удалось найти корневой объект префаба для {gameObject.name}.");
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"{gameObject.name} не является инстансом префаба.");
+            }
+#endif
+        }
+    }
+}
