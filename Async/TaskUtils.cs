1c5db18c54825b4e75ce07bfcf5376ff1e73b240
exerussus <solovyov.production@gmail.com>
2025-01-04 20:56:33 +0300

    Auto updated to version 1.0.51

---
+++ Async/TaskUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Exerussus._1Extensions.Async
+{
+    public static class TaskUtils
+    {
+        public static async Task WaitUntilAsync(Func<bool> condition, int checkInterval = 100)
+        {
+            while (!condition())
+            {
+                await Task.Delay(checkInterval);
+            }
+        }
+    }
+}
