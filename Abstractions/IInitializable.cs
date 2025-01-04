e9cb2985a727e9df0d983dc6b3fb4b9910411d01
exerussus <solovyov.production@gmail.com>
2025-01-04 19:59:16 +0300

    Auto updated to version 1.0.50

--- Abstractions/IInitializable.cs
+++ Abstractions/IInitializable.cs
@@ -1,5 +1,22 @@
-﻿namespace Exerussus._1Extensions.Abstractions
+﻿using Exerussus._1Extensions.SmallFeatures;
+
+namespace Exerussus._1Extensions.Abstractions
 {
+    public interface IInjectable
+    {
+        public void Inject(GameShare gameShare);
+    }
+
+    public interface IBeforeInitializable
+    {
+        public void BeforeInitialize();
+    }
+
+    public interface IPostInitializable
+    {
+        public void PostInitialize();
+    }
+    
     public interface IInitializable
     {
         public void Initialize();
