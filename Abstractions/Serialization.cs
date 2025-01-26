0fe8980a14415d1d1a6ff288edea6184bab9befb
exerussus <solovyov.production@gmail.com>
2025-01-26 17:26:56 +0300

    Auto updated to version 1.0.57

---
+++ Abstractions/Serialization.cs
@@ -0,0 +1,22 @@
+﻿namespace Exerussus._1Extensions.Abstractions
+{
+    public interface IOnDeserialized
+    {
+        public void OnDeserialized();
+    }
+    
+    public interface IOnBeforeSerialize
+    {
+        public void OnDeserialized();
+    }
+    
+    public interface IOnDeserialized<in T>
+    {
+        public void OnDeserialized(T reference);
+    }
+    
+    public interface IOnBeforeSerialize<in T>
+    {
+        public void OnDeserialized(T reference);
+    }
+}
