27fbc6cbfeea3180eeb3d9fe18bcee29f1e320e5
exerussus <solovyov.production@gmail.com>
2025-02-02 20:48:26 +0300

    Auto updated to version 1.0.66

---
+++ Serialization/KeyValue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Exerussus._1Extensions.Serialization
+{
+    [Serializable]
+    public class KeyValue<TKey, TValue>
+    {
+        public TKey key;
+        public TValue value;
+    }
+}
