bc61aaafef3d057e9f422764f218a28133ddcc8e
exerussus <solovyov.production@gmail.com>
2025-02-16 20:53:20 +0300

    Auto updated to version 1.0.77

--- SignalSystem/Signal.cs
+++ SignalSystem/Signal.cs
@@ -16,12 +16,27 @@ namespace Exerussus._1Extensions.SignalSystem
 
         private Dictionary<Type, object> _listeners = new Dictionary<Type, object>();
 
-        /// <summary>
-        /// Вызывает сигнал 
-        /// </summary>
+        /// <summary> Вызывает сигнал. </summary>
         public void RegistryRaise<T>(T data) where T : struct
         {
             var type = typeof(T);
+            if (IsLogEnabled) Debug.Log($"{type}");
+
+            if (_listeners.TryGetValue(type, out var actionList))
+            {
+                var actions = (List<Action<T>>)actionList;
+                for (var index = actions.Count - 1; index >= 0; index--)
+                {
+                    actions[index].Invoke(data);
+                }
+            }
+        }
+        
+        /// <summary> Вызывает сигнал без аргументов. </summary>
+        public void RegistryRaise<T>() where T : struct
+        {
+            var type = typeof(T);
+            var data = new T();
             if (IsLogEnabled) Debug.Log($"{type}");
 
             if (_listeners.TryGetValue(type, out var actionList))
