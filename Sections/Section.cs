ffd0ad16b4fee0d23852d110227c0b1c96dfd77a
exerussus <solovyov.production@gmail.com>
2025-02-19 13:37:16 +0300

    Auto updated to version 1.0.91

--- Sections/Section.cs
+++ Sections/Section.cs
@@ -1,13 +1,69 @@
 ﻿using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Exerussus._1Extensions.Sections
 {
     [Serializable]
     public class Section
     {
-        public Vector2 minPosition;
-        public Vector2 maxPosition;
-        public Vector2 centerPosition;
+        public Section(Vector2 leftBottom, Vector2 rightTop)
+        {
+            this.leftBottom = leftBottom;
+            this.rightTop = rightTop;
+        }
+
+        [FormerlySerializedAs("minPosition")] [SerializeField, ReadOnly] private Vector2 leftBottom;
+        [FormerlySerializedAs("maxPosition")] [SerializeField, ReadOnly] private Vector2 rightTop;
+        [SerializeField, ReadOnly] private Vector2 centerPosition;
+        
+        public Vector2 BottomCenter { get; private set; }
+        public Vector2 TopCenter { get; private set; }
+        public Vector2 LeftCenter { get; private set; }
+        public Vector2 RightCenter { get; private set; }
+        
+        public Vector2 LeftBottom
+        {
+            get => leftBottom;
+            set
+            {
+                leftBottom = value;
+                Recalculate();
+            }
+        }
+
+        public Vector2 RightTop
+        {
+            get => rightTop;
+            set
+            {
+                rightTop = value;
+                if (rightTop.x < leftBottom.x) rightTop.x = leftBottom.x;
+                if (rightTop.y < leftBottom.y) rightTop.y = leftBottom.y;
+                Recalculate();
+            }
+        }
+
+        public Vector2 CenterPosition => centerPosition;
+        
+#if UNITY_EDITOR
+        [Button]
+#endif
+        public void SetPoints(Vector2 leftBottomPoint, Vector2 rightTopPint)
+        {
+            leftBottom = leftBottomPoint;
+            rightTop = rightTopPint;
+            Recalculate();
+        }
+        
+        private void Recalculate()
+        {
+            centerPosition = leftBottom + (rightTop - leftBottom) / 2f;
+            BottomCenter = new Vector2(centerPosition.x, leftBottom.y);
+            TopCenter = new Vector2(centerPosition.x, rightTop.y);
+            LeftCenter = new Vector2(leftBottom.x, centerPosition.y);
+            RightCenter = new Vector2(rightTop.x, centerPosition.y);
+        }
     }
 }
--- Sections/SectionExtensions.cs
+++ Sections/SectionExtensions.cs
@@ -8,13 +8,24 @@ namespace Exerussus._1Extensions.Sections
         public static void DrawGizmos(this Section section, Color lineColor, Transform parent = null)
         {
             var offset = parent == null ? Vector2.zero : (Vector2)parent.position;
-            GizmosUtils.DrawQuad(offset + section.minPosition, offset + section.maxPosition, lineColor);
+            GizmosUtils.DrawQuad(offset + section.LeftBottom, offset + section.RightTop, lineColor);
         }
         
         public static void DrawGizmos(this Section section, Color lineColor, Color solidColor, Transform parent = null)
         {
             var offset = parent == null ? Vector2.zero : (Vector2)parent.position;
-            GizmosUtils.DrawSolidQuad(offset + section.minPosition, offset + section.maxPosition, lineColor, solidColor);
+            GizmosUtils.DrawSolidQuad(offset + section.LeftBottom, offset + section.RightTop, lineColor, solidColor);
+        }
+
+        public static bool ContainPosition(this Section section, Vector2 position)
+        {
+            return section.LeftBottom.x <= position.x && position.x <= section.RightTop.x &&
+                   section.LeftBottom.y <= position.y && position.y <= section.RightTop.y;
+        }
+
+        public static Vector2 GetRandomPosition(this Section section)
+        {
+            return new Vector2(Random.Range(section.LeftBottom.x, section.RightTop.x), Random.Range(section.LeftBottom.y, section.RightTop.y));
         }
     }
 }
