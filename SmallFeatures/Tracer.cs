a29045972bcf7fe379212e1f5167bd75fa6c67cb
exerussus <solovyov.production@gmail.com>
2025-02-22 20:25:55 +0300

    Auto updated to version 1.0.99

--- SmallFeatures/Tracer.cs
+++ SmallFeatures/Tracer.cs
@@ -1,32 +1,58 @@
-﻿using UnityEngine;
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Exerussus._1Extensions.SmallFeatures
 {
     public static class Tracer
     {
-        private const string DefaultPrefix = ">>>>>>>>>>>>>>> ";
-        private static string _prefix = DefaultPrefix;
-        
-        public static void Start(string prefix)
+        public const string DefaultPrefix = ">>>>>>>>>>>>>>> ";
+
+        private static readonly Queue<Trace> Traces = new();
+
+        public static Trace Start(string prefix = Tracer.DefaultPrefix)
         {
-            _prefix = prefix;
-            Debug.Log($"{_prefix}Start");
-        }
-        
-        public static void End()
-        {
-            Debug.Log($"{_prefix}End");
-            _prefix = DefaultPrefix;
+            if (!Traces.TryDequeue(out var trace)) trace = new Trace();
+            
+            trace.Prefix = prefix;
+            Debug.Log($"{trace.Prefix} | Tracing started.");
+            return trace;
         }
 
         public static void Ping(int message)
         {
-            Debug.Log($"{_prefix}{message}");
+            Debug.Log($"{DefaultPrefix}{message}");
         }
 
         public static void Ping(string message)
         {
-            Debug.Log($"{_prefix}{message}");
+            Debug.Log($"{DefaultPrefix}{message}");
+        }
+
+        public class Trace : IDisposable
+        {
+            public string Prefix;
+            private int _count;
+        
+            public Trace Ping(string message = "")
+            {
+                _count++;
+                Debug.Log($"{Prefix} | {_count}. {message}");
+                return this;
+            }
+
+            public void End()
+            {
+                Debug.Log($"{Prefix} | Tracing end.");
+                _count = 0;
+                Traces.Enqueue(this);
+            }
+        
+            public void Dispose()
+            {
+                End();
+            }
         }
     }
 }
