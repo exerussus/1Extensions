1e1b4e71c10134384135cd29bee2c4788fe3bcbc
exerussus <solovyov.production@gmail.com>
2025-02-22 20:11:45 +0300

    Auto updated to version 1.0.98

--- SmallFeatures/Tracer.cs
+++ SmallFeatures/Tracer.cs
@@ -4,12 +4,19 @@ namespace Exerussus._1Extensions.SmallFeatures
 {
     public static class Tracer
     {
-        private static string _prefix = ">>>>>>>>>>>>>>> ";
+        private const string DefaultPrefix = ">>>>>>>>>>>>>>> ";
+        private static string _prefix = DefaultPrefix;
         
         public static void Start(string prefix)
         {
             _prefix = prefix;
             Debug.Log($"{_prefix}Start");
+        }
+        
+        public static void End()
+        {
+            Debug.Log($"{_prefix}End");
+            _prefix = DefaultPrefix;
         }
 
         public static void Ping(int message)
