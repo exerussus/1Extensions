09763a1087b946b4f5c0e5f880cf557fd19fb660
exerussus <solovyov.production@gmail.com>
2025-03-04 00:26:50 +0300

    Auto updated to version 1.0.108

---
+++ Async/AsyncOperationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Exerussus._1Extensions.Async
+{
+    public static class AsyncOperationExtensions
+    {
+        public static async Task Await(this AsyncOperation operation)
+        {
+            if (operation == null)
+            {
+                Debug.LogError("Can't complete null operation.");
+                return;
+            }
+
+            if (operation.isDone) return;
+
+            var tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            void Complete(AsyncOperation op)
+            {
+                op.completed -= Complete;
+                tcs.TrySetResult(true);
+            }
+
+            operation.completed += Complete;
+
+            await tcs.Task;
+        }
+    }
+}
