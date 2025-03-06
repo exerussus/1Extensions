5eebfdb5f903f78f8e12ed70147a8c49cc1091d7
exerussus <solovyov.production@gmail.com>
2025-03-06 19:01:12 +0300

    Auto updated to version 1.0.109

---
+++ Serialization/NamedVector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Exerussus._1Extensions.Serialization
+{
+    [Serializable]
+    public class NamedVector2
+    {
+        public string name;
+        public Vector2 value;
+    }
+    
+    [Serializable]
+    public class NamedVector3
+    {
+        public string name;
+        public Vector3 value;
+    }
+}
