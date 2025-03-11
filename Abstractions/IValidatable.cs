197dda1538ce039470701511fb93bb6dc7052603
exerussus <solovyov.production@gmail.com>
2025-03-11 15:51:32 +0300

    Auto updated to version 1.0.116

---
+++ Abstractions/IValidatable.cs
@@ -0,0 +1,12 @@
+﻿namespace Exerussus._1Extensions.Abstractions
+{
+    public interface IValidatable
+    {
+        public bool IsValid();
+    }
+    
+    public interface IValidatable<in T>
+    {
+        public bool IsValid(T reference);
+    }
+}
