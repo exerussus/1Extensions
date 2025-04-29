cf9ee1928ca970be976a02655794f31a0d3682ab
exerussus <solovyov.production@gmail.com>
2025-04-29 23:40:24 +0300

    Auto updated to version 1.0.145

--- QoL/Editor/AssetsCreator.cs
+++ QoL/Editor/AssetsCreator.cs
@@ -13,12 +13,12 @@ namespace Exerussus._1Extensions.QoL.Editor
         /// <param name="fileName">Название файла, например, "SomeConfig".</param>
         /// <typeparam name="T">Тип Scriptable Object, который надо создать по пути.</typeparam>
         /// <returns>Итоговый AssetPath созданного Scriptable Object.</returns>
-        public static string CreateScriptableObject<T>(string folder, string fileName) where T : ScriptableObject
+        public static (bool isCreated, string fullPath) CreateScriptableObject<T>(string folder, string fileName) where T : ScriptableObject
         {
             var folders = PathUtils.GetIncrementalPathsWithFolderName(folder);
             var assetPath = $"{folder}/{fileName}.asset";
             var existingAsset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
-            if (existingAsset != null) return assetPath;
+            if (existingAsset != null) return (false,assetPath);
 
             foreach (var (folderPath, folderName) in folders)
             {
@@ -31,7 +31,7 @@ namespace Exerussus._1Extensions.QoL.Editor
             AssetDatabase.Refresh();
 
             Debug.Log($"{typeof(T).Name} создан по пути: " + assetPath);
-            return assetPath;
+            return (true, assetPath);
         }
 
         private static void CreateFolderIfNotExists(string parent, string folderName)
