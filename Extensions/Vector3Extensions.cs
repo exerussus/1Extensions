2470a825a82473987842a048a3294c57e2cc7099
exerussus <solovyov.production@gmail.com>
2025-05-14 22:30:40 +0300

    Auto updated to version 1.0.158

--- Extensions/Vector3Extensions.cs
+++ Extensions/Vector3Extensions.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Exerussus._1Extensions.Scripts.Extensions
 {
@@ -12,8 +13,43 @@ namespace Exerussus._1Extensions.Scripts.Extensions
         public static Vector3 Add(this Vector3 vector, float? x = null, float? y = null, float? z = null)
         {
             return new Vector3(vector.x + (x ?? 0),vector.y + ( y ?? 0), vector.z + ( z ?? 0));
-        } 
+        }
+        
+        public static string SerializeToString(this Vector3 vector)
+        {
+            return $"{vector.x},{vector.y},{vector.z}";
+        }
 
+        public static Vector3 DeserializeFromString(this string data)
+        {
+            var parts = data.Split(',');
+            if (parts.Length != 3) throw new FormatException("Invalid Vector3 string format.");
+            return new Vector3(
+                float.Parse(parts[0]),
+                float.Parse(parts[1]),
+                float.Parse(parts[2])
+            );
+        }
+
+        public static byte[] SerializeToBytes(this Vector3 vector)
+        {
+            var bytes = new byte[sizeof(float) * 3];
+            Buffer.BlockCopy(BitConverter.GetBytes(vector.x), 0, bytes, 0, sizeof(float));
+            Buffer.BlockCopy(BitConverter.GetBytes(vector.y), 0, bytes, sizeof(float), sizeof(float));
+            Buffer.BlockCopy(BitConverter.GetBytes(vector.z), 0, bytes, sizeof(float) * 2, sizeof(float));
+            return bytes;
+        }
+
+        public static Vector3 DeserializeFromBytes(this byte[] bytes)
+        {
+            if (bytes.Length != sizeof(float) * 3) throw new ArgumentException("Invalid byte array size for Vector3.");
+            return new Vector3(
+                BitConverter.ToSingle(bytes, 0),
+                BitConverter.ToSingle(bytes, sizeof(float)),
+                BitConverter.ToSingle(bytes, sizeof(float) * 2)
+            );
+        }
+        
         /// <summary>
         /// Возвращает квадрат расстояния между двумя векторами <see cref="Vector3"/>.
         /// </summary>
