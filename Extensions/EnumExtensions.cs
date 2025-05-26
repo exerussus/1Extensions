cb0f7a4bc6e4e8ae56b6e0d595d9ba8a2ae89953
exerussus <solovyov.production@gmail.com>
2025-05-26 17:11:08 +0300

    Auto updated to version 1.0.167

---
+++ Extensions/EnumExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exerussus._1Extensions.Scripts.Extensions
+{
+    public static class EnumExtensions
+    {
+        private static readonly Random _random = new();
+
+        /// <summary> Возвращает случайное значение для перечисления типа TEnum. </summary>
+        public static TEnum GetRandomValue<TEnum>() where TEnum : Enum
+        {
+            var values = (TEnum[])Enum.GetValues(typeof(TEnum));
+            return values[_random.Next(values.Length)];
+        }
+
+        /// <summary> Устанавливает случайное значение для переданного enum по ссылке. </summary>
+        public static void SetRandomValue<TEnum>(this ref TEnum enumValue) where TEnum : struct, Enum
+        {
+            enumValue = GetRandomValue<TEnum>();
+        }
+        
+        /// <summary> Увеличивает значение enum циклически (через ref), возвращаясь к первому при достижении конца. </summary>
+        public static void NextEnumValue<TEnum>(this ref TEnum current) where TEnum : struct, Enum
+        {
+            var values = (TEnum[])Enum.GetValues(typeof(TEnum));
+            var index = Array.IndexOf(values, current);
+            index = (index + 1) % values.Length;
+            current = values[index];
+        }
+    }
+}
