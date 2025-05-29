10b1004301a3d8f0611125c286a541fa1f63a0b8
exerussus <solovyov.production@gmail.com>
2025-05-29 21:03:08 +0300

    Auto updated to version 1.0.168

---
+++ ProjectCenter/Project.cs
@@ -0,0 +1,15 @@
+﻿namespace Exerussus._1Extensions.ProjectCenter
+{
+    public static class Project
+    {
+        public static readonly LoggerCore Logger = new ();
+        public static readonly ParserCore Parser = new ();
+        public static readonly QoLCore QoL = new ();
+        public static readonly UtilsCore Utils = new ();
+        
+        public class LoggerCore { }
+        public class ParserCore { }
+        public class QoLCore { }
+        public class UtilsCore { }
+    }
+}
