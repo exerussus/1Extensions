0415d073f1713865c3bbcdeff653acd2fab045d5
exerussus <solovyov.production@gmail.com>
2025-06-02 21:52:18 +0300

    Auto updated to version 1.0.177

---
+++ Serialization/Abstractions.cs
@@ -0,0 +1,14 @@
+﻿namespace Exerussus._1Extensions.Serialization
+{
+    public interface IPersistable
+    {
+        public void Save();
+        public void Load();
+    }
+    
+    public interface IPersistable<in TSaveRef, in TLoadRef>
+    {
+        public void Save(TSaveRef reference);
+        public void Load(TLoadRef reference);
+    }
+}
