8f2c48a6736bd301301b2b02dc428162d8fac0bf
exerussus <solovyov.production@gmail.com>
2025-06-02 22:12:08 +0300

    Auto updated to version 1.0.179

--- Serialization/Abstractions.cs
+++ Serialization/Abstractions.cs
@@ -1,4 +1,6 @@
-﻿namespace Exerussus._1Extensions.Serialization
+﻿using UnityEngine;
+
+namespace Exerussus._1Extensions.Serialization
 {
     public interface IPersistable
     {
@@ -10,5 +12,28 @@
     {
         public void Save(TSaveRef reference);
         public void Load(TLoadRef reference);
+    }
+
+    public abstract class Persistable<T> : IPersistable
+    {
+        public abstract string AssetFolder { get; }
+        
+        public void Save()
+        {
+            AssetSerializer.SavePersistent(this, AssetFolder);
+        }
+
+        public void Load()
+        {
+            var (result, loaded) = AssetSerializer.LoadPersistent<T>(AssetFolder);
+            if (result) OnLoad(loaded);
+            else
+            {
+                Debug.LogWarning("Failed to load. Creating and saving new.");
+                Save();
+            }
+        }
+
+        public abstract void OnLoad(T loaded);
     }
 }
