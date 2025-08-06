19ff76ea19e6efa59bb2fe7a60b64aa035535679
exerussus <solovyov.production@gmail.com>
2025-08-07 00:17:10 +0300

    Auto updated to version 1.0.216

--- DelayedActionsFeature/Methods/PrivateMethods.cs
+++ DelayedActionsFeature/Methods/PrivateMethods.cs
@@ -1,6 +1,5 @@
 ﻿
 using System;
-using UnityEngine;
 
 namespace Exerussus._1Extensions.DelayedActionsFeature
 {
--- DelayedActionsFeature/Models/DelayedActionHandle.cs
+++ DelayedActionsFeature/Models/DelayedActionHandle.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Runtime.CompilerServices;
+
 namespace Exerussus._1Extensions.DelayedActionsFeature
 {
     public partial class DelayedAction
@@ -13,30 +15,35 @@ namespace Exerussus._1Extensions.DelayedActionsFeature
             private int Id { get; }
 
             /// <summary> Принудительно выполняет задачу без ожидания, но с валидацией и проверкой. </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public void InvokeForce()
             {
                 DelayedAction.InvokeForceOperation(Id);
             }
             
             /// <summary> Запускает операцию, если она была остановлена. Задача запущена по-умолчанию. </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public void Start()
             {
                 DelayedAction.StartOperation(Id);
             }
             
             /// <summary> Останавливает операцию, не отменяя её. </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public void Stop()
             {
                 DelayedAction.StopOperation(Id);
             }
             
             /// <summary> Сбрасывает время задержки операции до стандартного ожидания, и делает операцию активной. </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public void Reset()
             {
                 DelayedAction.ResetOperation(Id);
             }
             
             /// <returns>True, если операция всё ещё выполняется. </returns>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public bool IsValid()
             {
                 return Id != 0 && (DelayedAction.ToCreate.ContainsKey(Id) || DelayedAction.DictInWork.ContainsKey(Id) && !DelayedAction.ToRelease.Contains(Id));
@@ -44,6 +51,7 @@ namespace Exerussus._1Extensions.DelayedActionsFeature
             
             /// <summary> Отменяет задачу. </summary>
             /// <returns>True, если операция была отменена. </returns>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public bool TryCancel()
             {
                 if (DelayedAction.DictInWork.ContainsKey(Id))
@@ -56,6 +64,7 @@ namespace Exerussus._1Extensions.DelayedActionsFeature
             }
             
             /// <summary> Отменяет задачу. </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public void Cancel()
             {
                 DelayedAction.PrepareToRelease(Id);
