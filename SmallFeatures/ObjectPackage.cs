b5e538827394157ed4e97c9837ebbd9406535b9d
Exerussus <solovyov.production@gmail.com>
2025-10-02 02:50:33 +0300

    updated

---
+++ SmallFeatures/ObjectPackage.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+
+namespace Exerussus._1Extensions.SmallFeatures
+{
+    public class ObjectPackage<T>
+    {
+        public T Value;
+    }
+
+    public static class ObjectPackageExtensions
+    {
+        public static async UniTask AwaitDone(this ObjectPackage<bool> package, int timeout = 20000, CancellationToken ct = default)
+        {
+            const int checkInterval = 100;
+            
+            while (!package.Value || !ct.IsCancellationRequested)
+            {
+                if (timeout <= 0) return;
+                await UniTask.Delay(checkInterval, cancellationToken: ct);
+                timeout -= checkInterval;
+            }
+        }
+    }
+}
