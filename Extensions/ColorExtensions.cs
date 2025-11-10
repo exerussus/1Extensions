3e94fbddd35efdf4d57ec36e07edd217677ed5a0
Exerussus <solovyov.production@gmail.com>
2025-11-10 17:41:21 +0300

    updated

---
+++ Extensions/ColorExtensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Exerussus._1Extensions.Scripts.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static Color WithAlpha(this Color color, float alpha) => new Color(color.r, color.g, color.b, alpha);
+    }
+}
